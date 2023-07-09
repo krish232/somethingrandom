5e76ad30902363480e387c0854f5a4ffb0afe9bd
krish232 <139031653+krish232@users.noreply.github.com>
2023-07-09 10:06:32 +0530

    Add files via upload

---
+++ If.java
@@ -0,0 +1,7 @@
+public class If {
+public static void main(String args[]) {
+  System.out.println("If black lives matter");
+  System.out.println("then name every black person in the world");
+  System.out.println("nigga 1 nigga 2 nigga 3");
+}
+}  
